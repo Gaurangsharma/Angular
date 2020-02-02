421a0a732082e86135f2de6e2149daa27e7a51ba
Gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2020-02-03 00:44:12 +0530

    RxJs Part 3

--- conFusion/dogit.bat
+++ conFusion/dogit.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "RxJs Part 2"
+git commit -m "RxJs Part 3"
 git push origin master
--- conFusion/src/app/contacts/contacts.component.ts
+++ conFusion/src/app/contacts/contacts.component.ts
@@ -23,17 +23,46 @@ export class ContactsComponent implements OnInit {
   ngOnInit() {
    
   }
+  formErrors={
+    'firstname':'',
+    'lastname':'',
+    'telnum':'',
+    'email':'',
+  };
+
+  validationMessages={
+    firstname:{
+      'required':'First Name is required',
+      'minlength':'First Name is atleast 2 char Long',
+      'maxlength':'First Name is can not be more than 25 char long.'
+    },
+    lastname:{
+      'required':'Last Name is required',
+      'minlength':'Last Name is atleast 2 char Long',
+      'maxlength':'Last Name is can not be more than 25 char long.'
+    },
+    telnum:{
+      'required':'Tel No. is required',
+      'pattern':'Tel No. must only contain only numbers.',
+    },
+    email:{
+      'required':'Email is required',
+      'email':'Email is not in valid formate',
+    },
+  }
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['',Validators.required],
-      lastname: ['',Validators.required],
-      telnum: [0,Validators.required],
-      email: ['',Validators.required],
+      firstname: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+      lastname: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+      telnum: [0,[Validators.required,Validators.pattern]],
+      email: ['',Validators.required,Validators.email],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges.subscribe(data=>this.onValueChanged(data));
   }
 
   onSubmit() {
@@ -49,6 +78,26 @@ export class ContactsComponent implements OnInit {
       message: ''
     });
     this.feedbackFormDirective.resetForm();
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
 }
