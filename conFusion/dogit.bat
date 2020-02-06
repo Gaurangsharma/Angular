dd8c52982eb4494feab99639766c0f1f642f2786
Gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2020-02-06 18:36:04 +0530

    Assignment upto task4 half

--- conFusion/dogit.bat
+++ conFusion/dogit.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "Assignment upto task3"
+git commit -m "Assignment upto task4 half"
 git push origin master
--- conFusion/src/app/contacts/contacts.component.ts
+++ conFusion/src/app/contacts/contacts.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { FormBuilder,FormGroup,Validator, Validators  } from "@angular/forms";
-import { Feedback,ContactType } from "../shared/feedback";
-import { visibility,flyInOut,expands } from "../animation/app.animation";
+import { FormBuilder, FormGroup, Validator, Validators } from "@angular/forms";
+import { Feedback, ContactType } from "../shared/feedback";
+import { visibility, flyInOut, expands } from "../animation/app.animation";
 import { FeedbackService } from "../services/feedback.service";
 import { expand } from 'rxjs/operators';
 
@@ -10,11 +10,11 @@ import { expand } from 'rxjs/operators';
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss'],
-  host:{
-    '[@flyInOut]':'true',
-    'style':'display:block;'
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display:block;'
   },
-  animations:[
+  animations: [
     flyInOut(),
     expands()
   ]
@@ -24,76 +24,91 @@ export class ContactsComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
-  feedbackcopy:Feedback;
+  feedbackcopy: Feedback[]=[];
+  showLoader: boolean = false;
 
-  
+
   constructor(private fb: FormBuilder,
-    private feedbackservice:FeedbackService) {
+    private feedbackservice: FeedbackService) {
     this.createForm();
   }
 
   ngOnInit() {
-   
+
   }
-  formErrors={
-    'firstname':'',
-    'lastname':'',
-    'telnum':'',
-    'email':'',
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': '',
   };
 
-  validationMessages={
-    firstname:{
-      'required':'First Name is required',
-      'minlength':'First Name is atleast 2 char Long',
-      'maxlength':'First Name is can not be more than 25 char long.'
+  validationMessages = {
+    firstname: {
+      'required': 'First Name is required',
+      'minlength': 'First Name is atleast 2 char Long',
+      'maxlength': 'First Name is can not be more than 25 char long.'
     },
-    lastname:{
-      'required':'Last Name is required',
-      'minlength':'Last Name is atleast 2 char Long',
-      'maxlength':'Last Name is can not be more than 25 char long.'
+    lastname: {
+      'required': 'Last Name is required',
+      'minlength': 'Last Name is atleast 2 char Long',
+      'maxlength': 'Last Name is can not be more than 25 char long.'
     },
-    telnum:{
-      'required':'Tel No. is required',
-      'pattern':'Tel No. must only contain only numbers.',
+    telnum: {
+      'required': 'Tel No. is required',
+      'pattern': 'Tel No. must only contain only numbers.',
     },
-    email:{
-      'required':'Email is required',
-      'email':'Email is not in valid formate',
+    email: {
+      'required': 'Email is required',
+      'email': 'Email is not in valid formate',
     },
   }
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
-      lastname: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
-      telnum: [0,[Validators.required,Validators.pattern]],
-      email: ['',Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', Validators.required],
       agree: false,
       contacttype: 'None',
       message: '',
     });
 
-    this.feedbackForm.valueChanges.subscribe(data=>this.onValueChanged(data));
+    this.feedbackForm.valueChanges.subscribe(data => this.onValueChanged(data));
   }
 
   onSubmit() {
+    this.showLoader = true;
     this.feedback = this.feedbackForm.value;
-    console.log(this.feedback+" Inside Onsubmit");
+    console.log(this.feedback + " Inside Onsubmit");
     this.feedbackservice.putFeedback(this.feedback)
-    .subscribe(feedback=>this.feedback=feedback);
-    console.log("Resetting");
+      .subscribe(feedback => {
+        console.log('------------------->>> ', feedback)
+        this.feedbackSubmitSuccess(feedback);
+        this.feedbackcopy.push(feedback);
+      });
+    
+    
+  }
+
+  feedbackSubmitSuccess(feedback) {
+    this.showLoader = false;
+    this.resetForm();
+  }
+
+  resetForm() {
     this.feedbackForm.reset({
-      firstname: '' ,
+      firstname: '',
       lastname: '',
-      telnum:  '',
-      email:  '',
+      telnum: '',
+      email: '',
       agree: false,
       contacttype: 'None',
       message: '',
 
     });
-  }
+  } 
 
   onValueChanged(data?: any) {
     if (!this.feedbackForm) { return; }
